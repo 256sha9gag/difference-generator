a40f8dfc4d19570bcc3269c131a0a2848e308590
gag <256sha9gag@gmail.com>
2022-09-27 22:27:44 +0300

    feat: add cli action

---
+++ cli/program.js
@@ -0,0 +1,19 @@
+import { Command } from 'commander';
+import getDiff from '../src/getdiff.js';
+import getObjs from '../src/getobjs.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    const result = getDiff(getObjs(filepath1, filepath2));
+    console.log(result);
+  })
+  .option('-f, --format <type>', 'output format');
+
+export default program;
