8bdf19273f38bd1c1f1663da2b765c85bec17b00
gag <256sha9gag@gmail.com>
2022-09-28 16:07:03 +0300

    refactor: eslint error

--- src/getobjs.js
+++ src/getobjs.js
@@ -2,7 +2,6 @@ import * as fs from 'node:fs';
 import { resolve } from 'node:path';
 import { cwd } from 'node:process';
 
-
 const getObjs = (filepath1, filepath2) => {
   const AbsPathFile1 = resolve(cwd(), filepath1);
   const AbsPathFile2 = resolve(cwd(), filepath2);
