67a8dad87db01fbce4479e7b9b7934cda0b1b449
gag <256sha9gag@gmail.com>
2022-09-28 21:43:18 +0300

    refactor: getDiff

--- cli/program.js
+++ cli/program.js
@@ -8,8 +8,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
+  .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const result = getDiff(getObjs(filepath1, filepath2));
     console.log(result);
