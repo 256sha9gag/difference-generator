e75a9fd906194f8e1c6d70b3e19ca149d5330306
gag <256sha9gag@gmail.com>
2022-09-28 13:32:44 +0300

    refactor: add new const

--- src/getdiff.js
+++ src/getdiff.js
@@ -5,22 +5,25 @@ const getDiff = (objs) => {
   const concatKeys = _.sortBy(_.uniq(Object.keys(objFile1).concat(Object.keys(objFile2))));
 
   const diffStr = concatKeys.reduce((acc, elem) => {
+    const elemObjFile1 = `"${elem}": ${objFile1[elem]}`;
+    const elemObjFile2 = `"${elem}": ${objFile2[elem]}`;
+
     if (_.has(objFile1, elem) && _.has(objFile2, elem)) {
       if (objFile1[elem] === objFile2[elem]) {
-        acc.push(`\n    "${elem}": ${objFile1[elem]}`);
+        acc.push(`\n    ${elemObjFile1}`);
       }
       if (objFile1[elem] !== objFile2[elem]) {
-        acc.push(`\n  - "${elem}": ${objFile1[elem]}`);
-        acc.push(`\n  + "${elem}": ${objFile2[elem]}`);
+        acc.push(`\n  - ${elemObjFile1}`);
+        acc.push(`\n  + ${elemObjFile2}`);
       }
     }
 
     if (_.has(objFile1, elem) && !_.has(objFile2, elem)) {
-      acc.push(`\n  - "${elem}": ${objFile1[elem]}`);
+      acc.push(`\n  - ${elemObjFile1}`);
     }
 
     if (!_.has(objFile1, elem) && _.has(objFile2, elem)) {
-      acc.push(`\n  + "${elem}": ${objFile2[elem]}`);
+      acc.push(`\n  + ${elemObjFile2}`);
     }
 
     return acc;
