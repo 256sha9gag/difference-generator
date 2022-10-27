aeaa8cfa35ff2002a232884d19738c822e799ab6
gag <256sha9gag@gmail.com>
2022-10-27 14:51:51 +0300

    test: add test for yml

--- __tests__/program.test.js
+++ __tests__/program.test.js
@@ -6,11 +6,16 @@ import { dirname } from 'path';
 import getDiff from '../src/getdiff.js';
 import trueResult from '../__fixtures__/trueResult.js';
 
-test('getobjs', () => {
-  const __filename = fileURLToPath(import.meta.url);
-  const __dirname = dirname(__filename);
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
+test('getdiff json', () => {
+  // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
   expect(trueResult).toBe(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json')));
+});
+
+test('getdiff yml', () => {
+  // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+  expect(trueResult).toBe(getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml')));
 });
