e2a7dc0ae4b204006e34344b1c7c5140dd9824ed
gag <256sha9gag@gmail.com>
2022-10-27 14:42:54 +0300

    refactor __tests__/program.test.js

--- __tests__/program.test.js
+++ __tests__/program.test.js
@@ -2,7 +2,7 @@ import * as path from 'path';
 import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import * as fs from 'node:fs';
+// import * as fs from 'node:fs';
 import getDiff from '../src/getdiff.js';
 import trueResult from '../__fixtures__/trueResult.js';
 
@@ -10,7 +10,7 @@ test('getobjs', () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
   const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-  const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+  // const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-  expect(trueResult).toBe(getDiff([readFile('file1.json'), readFile('file2.json')]));
+  expect(trueResult).toBe(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json')));
 });
