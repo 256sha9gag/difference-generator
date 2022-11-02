5cc1f14c8ac1b40c811145068309e4c2598a8add
gag <256sha9gag@gmail.com>
2022-11-02 14:18:56 +0300

    fix: float tests

--- __tests__/program.test.js
+++ __tests__/program.test.js
@@ -11,9 +11,9 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('getdiff json', () => {
-  expect(readFile('expect_nested.txt')).toBe(getDiff(getFixturePath('float_file1.json'), getFixturePath('float_file2.json')));
+  expect(readFile('expect_float.txt')).toBe(getDiff(getFixturePath('float_file1.json'), getFixturePath('float_file2.json')));
 });
 
 test('getdiff yml', () => {
-  expect(readFile('expect_nested.txt')).toBe(getDiff(getFixturePath('float_file1.yml'), getFixturePath('float_file2.yml')));
+  expect(readFile('expect_float.txt')).toBe(getDiff(getFixturePath('float_file1.yml'), getFixturePath('float_file2.yml')));
 });
