8a0cbb946cf4cfadd4a5479c2fad7aced8107145
gag <256sha9gag@gmail.com>
2022-12-15 17:01:39 +0300

    test: add nested object tests

--- __tests__/program.test.js
+++ __tests__/program.test.js
@@ -3,7 +3,7 @@ import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as fs from 'node:fs';
-import getDiff from '../src/getdiff.js';
+import getDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,10 +13,21 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8'
 const expectFloat = readFile('expect_float.txt');
 console.log(typeof expectFloat);
 
-test('getdiff json', () => {
+const expectNested = readFile('expect_nested.txt');
+console.log(typeof expectFloat);
+
+test('getdiff float json', () => {
   expect(getDiff(getFixturePath('float_file1.json'), getFixturePath('float_file2.json'))).toEqual(expectFloat);
 });
 
-test('getdiff yml', () => {
+test('getdiff float yml', () => {
   expect(getDiff(getFixturePath('float_file1.yml'), getFixturePath('float_file2.yml'))).toEqual(expectFloat);
+});
+
+test('getdiff nested json', () => {
+  expect(getDiff(getFixturePath('nested_file1.json'), getFixturePath('nested_file2.json'))).toEqual(expectNested);
+});
+
+test('getdiff nested yml', () => {
+  expect(getDiff(getFixturePath('nested_file1.yml'), getFixturePath('nested_file2.yml'))).toEqual(expectNested);
 });
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,66 @@
-const stylish = (value, replacer = ' ', spaceCount = 1) => {
-    const iter = (currentValue, depth) => {
-        currentValue.map(())
-    };
+import _ from 'lodash';
+
+const keyOffset = 4;
+const spacesCount = 2;
+const replacer = ' ';
+const label = {
+  nested: ' ',
+  added: '+',
+  deleted: '-',
+  unchange: ' ',
 };
+
+const addPrefix = (key, type, indent) => `${indent}${label[type]} ${key}`;
+
+const stringify = (value, depth) => {
+  if (!_.isObject(value)) {
+    return `${value}`;
+  }
+
+  const indentSize = depth * keyOffset;
+  const keyIndent = replacer.repeat(indentSize);
+  const bracketIndent = replacer.repeat(indentSize - keyOffset);
+  const lines = Object
+    .entries(value)
+    .map(([key, val]) => `${keyIndent}${key}: ${stringify(val, depth + 1)}`);
+
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
+};
+
+const stylish = (diff) => {
+  const iter = (tree, depth) => {
+    const indentSize = depth * keyOffset;
+    const keyIndent = replacer.repeat(indentSize - spacesCount);
+    const bracketIndent = replacer.repeat(indentSize);
+
+    return tree.flatMap((elem) => {
+      switch (elem.type) {
+        case 'added':
+        case 'deleted':
+        case 'unchange':
+          return `${addPrefix(elem.name, elem.type, keyIndent)}: ${stringify(elem.value, depth + 1)}`;
+        case 'changed':
+          return [
+            `${addPrefix(elem.name, 'deleted', keyIndent)}: ${stringify(elem.value1, depth + 1)}`,
+            `${addPrefix(elem.name, 'added', keyIndent)}: ${stringify(elem.value2, depth + 1)}`,
+          ];
+        case 'nested':
+          return [
+            `${addPrefix(elem.name, elem.type, keyIndent)}: {`,
+            ...iter(elem.children, depth + 1),
+            `${bracketIndent}}`,
+          ];
+        default:
+          throw new Error('unknown type node');
+      }
+    });
+  };
+  const lines = iter(diff, 1);
+  return ['{', ...lines, '}'].join('\n');
+};
+
+export default stylish;
