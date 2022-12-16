adda18cd17c158ac55c1d4fc16bbbb0741897a03
gag <256sha9gag@gmail.com>
2022-12-16 12:46:58 +0300

    feat: add plain formatter

---
+++ src/formatters/plain.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  if (_.isString(value)) {
+    return `'${value}'`;
+  }
+  return value;
+};
+
+const plain = (diff) => {
+  const iter = (tree, path) => tree.flatMap((elem) => {
+    const property = (path !== '') ? `${path}.${elem.name}` : `${elem.name}`;
+    switch (elem.type) {
+      case 'removed':
+        return `Property '${property}' was removed`;
+      case 'added':
+        return `Property '${property}' was added with value: ${stringify(elem.value)}`;
+      case 'updated':
+        return `Property '${property}' was updated. From ${stringify(elem.value1)} to ${stringify(elem.value2)}`;
+      case 'nested':
+        return iter(elem.children, property);
+      case 'unchanged':
+        return null;
+      default:
+        throw new Error('Unknown type node');
+    }
+  });
+  const lines = iter(diff, '');
+  return lines.filter((line) => line).join('\n');
+};
+
+export default plain;
