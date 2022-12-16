a18626da446f6833a8d1bb4038904b2b1e7b60d1
gag <256sha9gag@gmail.com>
2022-12-16 16:03:52 +0300

    refactor: stylush

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -36,7 +36,6 @@ const stylish = (diff) => {
     const indentSize = depth * keyOffset;
     const keyIndent = replacer.repeat(indentSize - spacesCount);
     const bracketIndent = replacer.repeat(indentSize);
-
     return tree.flatMap((elem) => {
       switch (elem.type) {
         case 'added':
@@ -44,10 +43,8 @@ const stylish = (diff) => {
         case 'unchanged':
           return `${addPrefix(elem.name, elem.type, keyIndent)}: ${stringify(elem.value, depth + 1)}`;
         case 'updated':
-          return [
-            `${addPrefix(elem.name, 'removed', keyIndent)}: ${stringify(elem.value1, depth + 1)}`,
-            `${addPrefix(elem.name, 'added', keyIndent)}: ${stringify(elem.value2, depth + 1)}`,
-          ];
+          return [`${addPrefix(elem.name, 'removed', keyIndent)}: ${stringify(elem.value1, depth + 1)}`,
+            `${addPrefix(elem.name, 'added', keyIndent)}: ${stringify(elem.value2, depth + 1)}`];
         case 'nested':
           return [
             `${addPrefix(elem.name, elem.type, keyIndent)}: {`,
@@ -59,8 +56,7 @@ const stylish = (diff) => {
       }
     });
   };
-  const lines = iter(diff, 1);
-  return ['{', ...lines, '}'].join('\n');
+  return ['{', ...iter(diff, 1), '}'].join('\n');
 };
 
 export default stylish;
